1fd5e73fe878261ea71222a3912a5a269286db43
fawda123 <mbafs2012@gmail.com>
2017-02-01 10:22:06 -0600

    working on ESCO revisions, scalebar and contours fig 1

--- figs/batch_conv.bat
+++ figs/batch_conv.bat
@@ -5,6 +5,6 @@ REM otherwise convert is confused with a window command
 REM files in the directory are converted from pdf to tif
 
 SETLOCAL EnableDelayedExpansion
-SET IMCONV="C:\Program Files\ImageMagick-7.0.2-Q16\Convert"
+SET IMCONV="C:\Program Files\ImageMagick-7.0.4-Q8\Convert"
 
 FOR %%f IN (*.pdf) do %IMCONV% -units PixelsPerInch -density 300 -compress LZW "%%~f" "%%~dpnf.tif"
